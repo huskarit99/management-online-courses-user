7081a13c1151b5bd607f71b45119677921e6eea5
minhquoc5599 <minhquoc5599@gmail.com>
2021-01-06 16:00:24 +0700

    add: login, register view

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,16 @@ router.get('/list-courses', function(req, res, next) {
     res.render('courses/list-courses', { title: 'Express' });
 });
 
+router.get('/course-detail', function(req, res, next) {
+    res.render('courses/course-detail', { title: 'Express' });
+});
+
+
+router.get('/login', function(req, res, next) {
+    res.render('users/login', { title: 'Express' });
+});
+
+router.get('/register', function(req, res, next) {
+    res.render('users/register', { title: 'Express' });
+});
 module.exports = router;
