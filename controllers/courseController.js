be38c58bcb78d5e92f287bfd155ce4972bcca1d9
huskarit99 <nguyenthaihocxm1999@gmail.com>
2021-01-19 21:42:53 +0700

    MOD: fix course-detail again

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -191,13 +191,13 @@ exports.course_detail = (req, res, next) => {
                 }
             }
             let course_popular = [];
-            if (course.length < 5) {
-                for (var i = 0; i <= 4; i++) {
+            if (course.length <= 5) {
+                for (var i = 0; i < course.length; i++) {
                     course[i].price = new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(course[i].price);
                     course_popular.push(course[i]);
                 }
             } else {
-                for (var i = 0; i <= 4; i++) {
+                for (var i = 0; i < 5; i++) {
                     course[i].price = new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(course[i].price);
                     course_popular.push(course[i]);
                 }
